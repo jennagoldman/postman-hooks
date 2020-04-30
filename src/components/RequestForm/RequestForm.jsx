130e99a618fb559172336cba46ed76cb5012c134
Jenna Goldman <jenna.goldman89@gmail.com>
2020-04-30 15:06:24 -0700

    add state for method and add onChange and checked attributes to radio inputs

--- src/components/RequestForm/RequestForm.jsx
+++ src/components/RequestForm/RequestForm.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const RequestForm = ({ url, onRequestSubmit, onUrlChange }) => (
+const RequestForm = ({ url, method, onRequestSubmit, onMethodChange, onUrlChange }) => (
   <form onSubmit={onRequestSubmit}>
     <input type="text" name="requestUrl" value={url} onChange={onUrlChange} />
-    <label htmlFor="get">Get</label>
-    <input type="radio" name="method" value="get" id="get" />
-    <label htmlFor="get">Put</label>
-    <input type="radio" name="method" value="put" id="put" />
-    <label htmlFor="get">Post</label>
-    <input type="radio" name="method" value="post" id="post" />
-    <label htmlFor="get">Delete</label>
-    <input type="radio" name="method" value="delete" id="delete" />
+    <label htmlFor="get">Get
+      <input type="radio" name="method" value="get" id="get" onChange={onMethodChange} checked={method === 'get'} />
+    </label>
+    <label htmlFor="put">Put
+      <input type="radio" name="method" value="put" id="put" onChange={onMethodChange} checked={method === 'put'}/>
+    </label>
+    <label htmlFor="post">Post
+      <input type="radio" name="method" value="post" id="post" onChange={onMethodChange} checked={method === 'post'}/>
+    </label>
+    <label htmlFor="delete">Delete
+      <input type="radio" name="method" value="delete" id="delete" onChange={onMethodChange} checked={method === 'delete'} />
+    </label>
     <button>Submit</button>
   </form>
 );
 
 RequestForm.propTypes = {
   url: PropTypes.string.isRequired,
+  method: PropTypes.string.isRequired,
   onUrlChange: PropTypes.func.isRequired,
-  onRequestSubmit: PropTypes.func.isRequired
+  onMethodChange: PropTypes.func.isRequired,
+  onRequestSubmit: PropTypes.func.isRequired,
 };
 
 export default RequestForm;
--- src/containers/RequestFormContainer/RequestFormContainer.jsx
+++ src/containers/RequestFormContainer/RequestFormContainer.jsx
@@ -3,18 +3,23 @@ import RequestForm from '../../components/RequestForm/RequestForm.jsx';
 
 const RequestFormContainer = () => {
   const [url, setUrl] = useState('');
+  const [method, setMethod] = useState('');
 
   const handleUrlChange = ({ target }) => {
     setUrl(target.value);
   };
 
+  const handleMethodChange = ({ target }) => {
+    setMethod(target.value);
+  };
+
   const handleRequestSubmit = (event) => {
     event.preventDefault();
-    console.log(url);
+    console.log('url: ', url, ', method: ', method);
   };
 
   return (
-    <RequestForm url={url} onUrlChange={handleUrlChange} onRequestSubmit={handleRequestSubmit} />
+    <RequestForm url={url} onUrlChange={handleUrlChange} method={method} onMethodChange={handleMethodChange} onRequestSubmit={handleRequestSubmit} />
   );
 };
 
