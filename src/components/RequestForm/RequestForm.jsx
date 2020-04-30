4cb288e2ca62b3838dc2c88824b5983cfd71f165
Jenna Goldman <jenna.goldman89@gmail.com>
2020-04-30 13:25:04 -0700

    add radio input for request method to Form component

--- src/components/RequestForm/RequestForm.jsx
+++ src/components/RequestForm/RequestForm.jsx
@@ -4,6 +4,10 @@ import PropTypes from 'prop-types';
 const RequestForm = ({ url, onRequestSubmit, onUrlChange }) => {
   <form onSubmit={onRequestSubmit}>
     <input type="text" name="requestUrl" value={url} onChange={onUrlChange} />
+    <input type="radio" name="method" value="get" />
+    <input type="radio" name="method" value="put" />
+    <input type="radio" name="method" value="post" />
+    <input type="radio" name="method" value="delete" />
     <button>Submit</button>
   </form>;
 };
---
+++ src/containers/RequestFormContainer/RequestFormContainer.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import RequestForm from '../../components/RequestForm/RequestForm.jsx';
+
+const RequestFormContainer = () = {
+  const [url, setUrl] = useState('');
+
+  const handleUrlChange =({ target })
+  return (
+    <RequestForm />
+  )
+}
