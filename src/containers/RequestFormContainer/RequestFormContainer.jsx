5127b5f72016adf1dfa8c63367cb949189b6463b
Jenna Goldman <jenna.goldman89@gmail.com>
2020-04-30 17:09:02 -0700

    display response in Response component

--- src/containers/RequestFormContainer/RequestFormContainer.jsx
+++ src/containers/RequestFormContainer/RequestFormContainer.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import RequestForm from '../../components/RequestForm/RequestForm.jsx';
+import Response from '../../components/Response/Response';
 
 const RequestFormContainer = () => {
   const [url, setUrl] = useState('');
   const [method, setMethod] = useState('');
   const [body, setBody] = useState('');
+  const [response, setResponse] = useState([]);
 
   const handleUrlChange = ({ target }) => {
     setUrl(target.value);
@@ -22,11 +24,14 @@ const RequestFormContainer = () => {
     event.preventDefault();
     fetch(url)
       .then(res => res.json())
-      .then(json => console.log(json));
+      .then(json => setResponse(JSON.stringify(json)));
   };
 
   return (
-    <RequestForm url={url} onUrlChange={handleUrlChange} method={method} onMethodChange={handleMethodChange} body={body} onBodyChange={handleBodyChange} onRequestSubmit={handleRequestSubmit} />
+    <>
+      <RequestForm url={url} onUrlChange={handleUrlChange} method={method} onMethodChange={handleMethodChange} body={body} onBodyChange={handleBodyChange} onRequestSubmit={handleRequestSubmit} />
+      <Response response={response} />
+    </>
   );
 };
 
--- src/containers/ResponseContainer/ResponseContainer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Response from '../../components/Response/Response.jsx';
-
-const ResponseContainer = () => (
-  <Response response={response} />
-);
-
-
-export default ResponseContainer;
